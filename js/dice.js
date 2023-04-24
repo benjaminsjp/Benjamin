a0937da0949b94ca35da0f32fac02f852f866a37
benjaminsjp <114567951+benjaminsjp@users.noreply.github.com>
2023-04-24 12:42:37 +0200

    Lagt til knapp i dice game

--- js/dice.js
+++ js/dice.js
@@ -15,6 +15,7 @@
 // document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
+/*
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -33,6 +34,9 @@ var randomImageSource2 = "../img/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
+
+
+
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Spiller 1 Vant!";
@@ -43,6 +47,35 @@ else if (randomNumber2 > randomNumber1) {
 else {
   document.querySelector("h1").innerHTML = "Uavgjort!";
 }
+*/
+
+function myFunction() {
+ 
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; // A random number between 1 and 6.
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1; // A random number between 1 and 6.
+
+
+  if (randomNumber1 == randomNumber2) {
+      document.querySelector("h1").innerHTML = "Uavgjort!";
+  } else if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩  " + "Spiller 1 vinner!";
+  } else if (randomNumber1 < randomNumber2) {
+      document.querySelector("h1").innerHTML = "Spiller 2 vinner!" + "  🚩";
+  }
+
+  // Use DOM to target Player 1's dice image. Set attribute by concactonating with randomNumber1.
+  document.getElementById("img1").setAttribute("src", "../img/dice" + randomNumber1 + ".png");
+
+  // Use DOM to target Player 2's dice image. Set attribute by concactonating with randomNumber2.
+  document.getElementById("img2").setAttribute("src", "../img/dice" + randomNumber2 + ".png");
+}
+
+// Triggered by a clicking event, run myFunction() and change the h1 according to the outcome. 
+document.getElementById("buttonDice").addEventListener("click", myFunction);
+
+
+
+
 
 
 //Kart
